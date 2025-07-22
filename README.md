24e7dca2d5b5f42d45457ef4b92c882f1d3471d5
soyWXY <johnny852321@gapp.nthu.edu.tw>
2025-07-22 05:53:15 +0000

    upload aes template project

---
+++ README.md
@@ -0,0 +1,5 @@
+# TEE LAB
+
+## 1. 在Release區下載映像檔，並匯入virtualbox
+
+## 2. 開啟VM後，打開家目錄下的aes資料夾
--- build-image/scripts/setup-repo.sh
+++ build-image/scripts/setup-repo.sh
@@ -4,7 +4,9 @@ mkdir optee
 cd optee
 repo init -u https://github.com/OP-TEE/manifest.git -m qemu_v8.xml
 repo sync
-mv optee_examples/aes ~
+rm -rf optee_examples/aes
 cd build
 make toolchains
 make
+
+git clone https://github.com/NTHU-SCOPELAB/cr-tee-image.git ~/
